93f59b1f8899b1f42c893a19d7fb88c23c70ed84
klitko <zheltovodets@ukr.net>
2016-12-02 17:03:54 +0200

    done!

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 /**
  * Created by klitko on 02.12.16.
  */
+
+
+var element = document.querySelectorAll('div');
+var i = 0;
+var timer;
+
+element[0].onclick = start_stop;
+var flags = true;
+var flag = true;
+
+start();
+
+function start() {
+    timer = setInterval(function () {
+        if (flags) {
+            go(i++);
+            i > 500 ? flags = false : '';
+        } else {
+            go(i--);
+            i < 0 ? flags = true : '';
+        }
+    }, 5);
+}
+
+function start_stop() {
+    if (flag) {
+        clearInterval(timer);
+        flag = false
+    } else {
+        start();
+        flag = true
+    }
+
+}
+
+function go(argument) {
+    element[0].style.marginLeft = argument + 'px';
+}
+
+
